227732ea4302926936cda5dba4a32c7aab10b1cf
softwaredevemmanuel <48366297+softwaredevemmanuel@users.noreply.github.com>
2023-10-09 15:48:04 +0100

    login and register

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
 const Register = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            <h1>Register for me</h1>
+            <form>
+                <h1>First Name:</h1>
+                <input placeholder='First Name' />
+                <br />
+                <h1>Last Name:</h1>
+                <input placeholder='Last Name' />
+                <br />
+                <h1>Email Address</h1>
+                <input placeholder='Email' />
+                <br />
+                <h1>Password</h1>
+                <input placeholder='Password' />
+            </form>
+
+
+            Have an account? <Link to='/login'>Login Here</Link>
+        </div>
+    )
 }
 
 export default Register
